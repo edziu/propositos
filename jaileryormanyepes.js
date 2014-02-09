853764863a6d9a714c652753cbc504d0915713b2
jailer yepes <jaiyoryep@hotmail.com>
2014-02-09 13:49:49 -0500

    Mi Proposito
    Gracias @mejorandola

---
+++ jaileryormanyepes.js
@@ -0,0 +1,8 @@
+Mi Propósito en este nuevo año
+
+Hola que tal!
+
+Mi gran propósito en este nuevo año es aprovechar al 100% todos los conocimientos que me brinda la comunidad mejorando.la para convertirme en uno de los mejores frontends de mi pais, logrando asi impulsar mi marca en desarrollo web generando un excelente servicio en tecnología para mi ciudad, ademas de compartir fuertemente mis conocimientos para mejorar, enseñar y aprender sobre este gran mundo de la Internet.
+
+Jailer Yorman Yepes Pino
+Medellin - Colombia
